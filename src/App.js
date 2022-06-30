ea1384417e621a4a7cacf4325cff319910b7b88f
anujpanwar01 <anujpanwar7911@gmail.com>
2022-06-30 08:25:05 +0530

    Done with gallery and also add skeleton for every img

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { GlobalStyle } from "./global-styles/global.styles";
 
 import Spinner from "./components/spinner/Spinner";
 import { createPortal } from "react-dom";
+import ImgDetail from "./components/imgDetail/ImgDetail";
+import { useSelector } from "react-redux";
 
 const NewsPage = React.lazy(() => import("./pages/news-page/News.Page"));
 const WeatherPage = React.lazy(() =>
@@ -19,6 +21,21 @@ const PageNotFound = React.lazy(() =>
 );
 
 function App() {
+  const gallery = useSelector((state) => state.gallery);
+  const [images, setImages] = useState([]);
+
+  const filterImg = gallery.filterImg;
+  useEffect(() => {
+    if (images.length === 0) {
+      setImages(filterImg);
+      console.log("no-rerender");
+    } else {
+      console.log("rerender");
+      const newVal = filterImg.slice(images.length);
+      setImages((prvImg) => [...prvImg, ...newVal]);
+    }
+  }, [filterImg]);
+
   const loadingSpinner = createPortal(
     <div
       className="spinner"
@@ -41,7 +58,17 @@ function App() {
         <Route path="/weather" element={<WeatherPage />} />
         <Route path="/covid" element={<CovidPage />} />
         <Route path={"/"} element={<NewsPage />} />
-        <Route path="/gallery" element={<RandomGallery />} />
+        <Route
+          path="/gallery"
+          element={
+            <RandomGallery
+              images={images}
+              notification={gallery.notification}
+            />
+          }
+        />
+        <Route path="gallery/:imgId" element={<ImgDetail img={images} />} />
+
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </React.Suspense>
@@ -49,3 +76,7 @@ function App() {
 }
 
 export default App;
+// function ImgDetail() {
+//   const { imgId } = useParams();
+//   return <p>{imgId}</p>;
+// }
