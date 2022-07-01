920e9c8b172b725bef11f2c12d407d2e6a2f63b1
anujpanwar01 <anujpanwar7911@gmail.com>
2022-07-01 11:20:01 +0530

    remove the .env file

--- src/pages/random-gallery/RandomGallery.jsx
+++ src/pages/random-gallery/RandomGallery.jsx
@@ -1,6 +1,54 @@
+import { useEffect, useState, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchImgHandler } from "../../store/gallery/gallery.action";
 import { RandomGalleryContainer } from "./RandomGallery.styles";
 
+const API_KEY = process.env.REACT_APP_UNSPLASH_API;
+
 const RandomGallery = () => {
-  return <RandomGalleryContainer>random</RandomGalleryContainer>;
+  const dispatch = useDispatch();
+  const gallery = useSelector((state) => state.gallery);
+  const [page, setPage] = useState(1);
+  const [element, setElement] = useState(null);
+
+  const observer = useRef(
+    new IntersectionObserver(
+      (entries) => {
+        const first = entries[0];
+        if (first.isIntersecting) {
+          setPage((prevPage) => prevPage + 1);
+        }
+      },
+      { threshold: 0.3, rootMargin: "50px" }
+    )
+  );
+
+  useEffect(() => {
+    const currentElement = element;
+    const currentObserver = observer.current;
+
+    if (currentElement) {
+      currentObserver.observe(currentElement);
+    }
+
+    return () => {
+      if (currentElement) {
+        currentObserver.unobserve(currentElement);
+      }
+    };
+  }, [element]);
+
+  useEffect(() => {
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=office&client_id=${API_KEY}`;
+    dispatch(fetchImgHandler(url));
+  }, [dispatch, page]);
+  return (
+    <RandomGalleryContainer>
+      <section>jlsdf</section>
+      <section className="sec" ref={setElement}>
+        sd
+      </section>
+    </RandomGalleryContainer>
+  );
 };
 export default RandomGallery;
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import covidSlice from "./covid/covid.slice";
+import gallerySlice from "./gallery/gallery.slice";
 import weatherSlice from "./weather/weather.slice";
 const store = configureStore({
   reducer: {
     weather: weatherSlice.reducer,
     covid: covidSlice.reducer,
+    gallery: gallerySlice.reducer,
   },
 });
 export default store;
